86fe1fe44bd1994a20d29f16ad38d27c70b5a398
dashvlas <1998dasha1998@inbox.ru>
2016-11-19 08:10:31 +0300

    Initial Commit

---
+++ ObjC Runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjC Runtime
+//
+//  Created by Dasha Korneichuk on 11/19/16.
+//  Copyright © 2016 Dasha Korneichuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjC Runtime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ObjC Runtime
+//
+//  Created by Dasha Korneichuk on 11/19/16.
+//  Copyright © 2016 Dasha Korneichuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
